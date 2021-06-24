9e78577baf0200079c47a7b943377279260a518b
liqi <1012274457@qq.com>
2021-06-24 22:29:25 +0800

    优化混入中的代码

--- src/common/mixin.js
+++ src/common/mixin.js
@@ -3,14 +3,15 @@ import { debounce } from 'common/utils'
 export const itemListenerMixin = {
   data() {
     return {
-      itemImageListener: null
+      itemImageListener: null,
+      refresh: null
     }
   },
   mounted() {
-    const refresh = debounce(this.$refs.scroll.refresh, 50)
+    this.refresh = debounce(this.$refs.scroll.refresh, 50)
     // 对监听的事件进行保存
     this.itemImageListener = () => {
-      refresh()
+      this.refresh()
     }
     this.$bus.on('itemImageLoad', this.itemImageListener)
   },
