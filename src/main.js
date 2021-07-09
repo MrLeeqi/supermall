fa1c63e0d40eed232c4727860a5b2b4f698ae6d3
liqi <1012274457@qq.com>
2021-07-10 06:51:46 +0800

    解决移动端300ms延迟

--- src/main.js
+++ src/main.js
@@ -1,17 +1,24 @@
 import { createApp } from 'vue'
-import mitt from 'mitt'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import mitt from 'mitt'
+import FastClick from 'fastclick';
 
 import toast from 'components/common/toast'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+
+// 添加事件总线对象
+app.config.globalProperties.$bus = new mitt()
+
 // 安装自定义插件toast
 app.use(toast)
 
-app.config.globalProperties.$bus = new mitt()
+// 解决移动端300ms延迟
+FastClick.attach(document.body)
 
 app.mount('#app')
